818bac80eff97bbd4ad6b2f13850af77e111b93c
shivani_simpleDev <shivi.solanki29@gmail.com>
2022-11-30 17:06:13 +0100

    on track

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,7 +2,7 @@ import useStateContext, { stateContext } from '../hooks/useStateContext'
 
 export default function Quiz() {
   const {context, setContext} = useStateContext(stateContext)
-  
+  ///ghjdf
   // setContext({ timeTaken: 1 })
   console.log(context);
   return (
